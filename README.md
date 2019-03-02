005ddad065d2d3f0a9c02387402d05b9528dc797
shanirub <shani.rubovitch@gmail.com>
2019-03-02 20:24:00 +0100

    working

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-getsum
+Beginner Series #3 Sum of Numbers
+codewars kata
+
+
+Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
---
+++ main.js
@@ -0,0 +1,21 @@
+function GetSum( a,b )
+{
+   if (a == b) {
+       return a    
+   }
+   
+   let sum = 0
+
+   if (a > b) {
+       for(let i = b; i <= a; i++) {
+           sum += i
+       }
+       return sum
+   }
+
+   for(let i = a; i <= b; i++) {
+       sum += i
+   }
+   return sum
+
+}
